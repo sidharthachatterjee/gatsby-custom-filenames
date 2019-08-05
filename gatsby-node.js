1214bd92479c04f7222538ed78d5750d3629c391
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-08-05 14:37:03 +0530

    Custom js filenames

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+const merge = require("webpack-merge")
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  function getOutput() {
+    switch (stage) {
+      case `develop`:
+        return {
+          filename: `[id].js`,
+        }
+      case `build-javascript`:
+        return {
+          filename: `[contenthash].js`,
+          chunkFilename: `[contenthash].js`,
+        }
+    }
+  }
+  actions.replaceWebpackConfig(merge(getConfig(), { output: getOutput() }))
+}
